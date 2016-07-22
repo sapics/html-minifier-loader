62780edbf23f6324b3fea822e3e30819f60cb15f
sapics <gv.nishino@gmail.com>
2016-07-22 17:15:21 +0900

    Enabled both html-minifier-loader option and query option

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var minify = require('html-minifier').minify
-var loaderUtils = require('loader-utils')
+  , loaderUtils = require('loader-utils')
 
 module.exports = function(source) {
   this.cacheable && this.cacheable()
-  var options = this.options['html-minifier-loader'] || loaderUtils.parseQuery(this.query)
   var defaultOptions = {
-    removeComments: true,
-    collapseWhitespace: true
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    , options = this.options['html-minifier-loader'] || {}
+    , options2
+  try {
+    options2 = loaderUtils.parseQuery(this.query)
+  } catch(e) {
+    options2 = {}
   }
-
-  return minify(source, Object.assign({}, defaultOptions, options))
+  return minify(source, Object.assign(defaultOptions, options, options2))
 }
